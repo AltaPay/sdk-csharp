41dfd9fe3a58679cb2ece50665eea9677b8649e4
anton <al@altapay.com>
2009-12-14 10:27:20 +0000

    Added some com+ attributes to the objects. hoping it helps

--- PensioMoto/PensioMoto/Moto.cs
+++ PensioMoto/PensioMoto/Moto.cs
@@ -10,6 +10,7 @@ using PensioMoto.Service.Dto;
 namespace PensioMoto
 {
 	[ComVisible(true)]
+	[GuidAttribute("81dac6af-1e5a-4327-a9ef-18a6f180627c")]
 	public class Moto
 	{
 		MotoDialog _dialog;
@@ -19,16 +20,19 @@ namespace PensioMoto
 			_dialog = new MotoDialog(new MotoForm(), new MerchantApi());
 		}
 
+		[DispIdAttribute(1)]
 		public void Initialize(string gatewayUrl, string apiUsername, string apiPassword, string terminal, string orderId, double amount, int currency, string paymentType)
 		{
 			_dialog.Initialize(gatewayUrl, apiUsername, apiPassword, terminal, orderId, amount, currency, (PaymentType)Enum.Parse(typeof(PaymentType), paymentType));
 		}
 
+		[DispIdAttribute(2)]
 		public void AddCreditCard(string maskedPan, string cardToken)
 		{
 			_dialog.AddCreditCard(maskedPan, cardToken);
 		}
 
+		[DispIdAttribute(3)]
 		public ComPaymentResult Show()
 		{
 			return new ComPaymentResult(_dialog.Show());
@@ -36,10 +40,14 @@ namespace PensioMoto
 	}
 
 	[ComVisible(true)]
+	[GuidAttribute("5f0ff6df-4bab-47d9-b53c-272cdd64bf69")]
 	public class ComPaymentResult
 	{
+		[DispIdAttribute(1)]
 		public string Result { get; set; }
+		[DispIdAttribute(2)]
 		public string ResultMessage { get; set; }
+		[DispIdAttribute(3)]
 		public Payment Payment { get; set; }
 
 		public ComPaymentResult(PaymentResult result)
--- PensioMoto/PensioMoto/Service/Dto/Payment.cs
+++ PensioMoto/PensioMoto/Service/Dto/Payment.cs
@@ -6,19 +6,27 @@ using System.Runtime.InteropServices;
 namespace PensioMoto.Service.Dto
 {
 	[ComVisible(true)]
+	[GuidAttribute("d5f7be20-5d6f-4690-8b2e-ac60d1f37c5a")]
 	public class Payment
 	{
 		[XmlElement(ElementName = "TransactionId")]
+		[DispIdAttribute(1)]
 		public int PaymentId { get; set; }
+		[DispIdAttribute(2)]
 		public string ShopOrderId { get; set; }
+		[DispIdAttribute(3)]
 		public string Terminal { get; set; }
 
 		[XmlElement(ElementName = "TransactionStatus")]
+		[DispIdAttribute(4)]
 		public string PaymentStatus { get; set; }
+		[DispIdAttribute(5)]
 		public string CreditCardToken { get; set; }
+		[DispIdAttribute(6)]
 		public string CreditCardMaskedPan { get; set; }
 
 		[XmlIgnore]
+		[DispIdAttribute(7)]
 		public Decimal ReservedAmount 
 		{
 			get
@@ -32,9 +40,11 @@ namespace PensioMoto.Service.Dto
 			}
 		}
 		[XmlElement(ElementName = "ReservedAmount")]
+		[DispIdAttribute(8)]
 		public string ReservedAmountString;
 
 		[XmlIgnore]
+		[DispIdAttribute(9)]
 		public Decimal CapturedAmount
 		{
 			get
@@ -47,6 +57,7 @@ namespace PensioMoto.Service.Dto
 			}
 		}
 		[XmlElement(ElementName = "CapturedAmount")]
+		[DispIdAttribute(10)]
 		public string CapturedAmountString;
 	}
 }
