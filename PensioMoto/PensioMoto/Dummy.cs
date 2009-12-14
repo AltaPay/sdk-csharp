1b2f2628fb49d4835011479b9366e41ac24954ec
anton <al@altapay.com>
2009-12-14 11:04:46 +0000

    Added dummy com+ object

---
+++ PensioMoto/PensioMoto/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace PensioMoto
+{
+	[ComVisible(true)]
+	[ClassInterface(ClassInterfaceType.None)]
+	[GuidAttribute("636dd551-a635-4334-b793-8eaae5efa4ff")]
+	public class Dummy
+	{
+		[DispIdAttribute(1)]
+		public int MyMethod(int i)
+		{
+			return i + 1;
+		}
+	}
+}
