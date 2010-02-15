544517f5affba39d7d97f58568bc4ee95ba33fca
emanuel <eg@altapay.com>
2010-02-15 21:01:53 +0000

    Added the RefundedAmount and CardStatus to IPayment too.

--- PensioMoto/PensioMoto/Com/IPayment.cs
+++ PensioMoto/PensioMoto/Com/IPayment.cs
@@ -13,9 +13,11 @@ namespace PensioMoto
 		string ShopOrderId { get; set; }
 		string Terminal { get; set; }
 		string PaymentStatus { get; set; }
+		string CardStatus { get; set; }
 		string CreditCardToken { get; set; }
 		string CreditCardMaskedPan { get; set; }
 		double ReservedAmount { get; set; }
 		double CapturedAmount { get; set; }
+		double RefundedAmount { get; set; }
 	}
 }
