80c9eaa5cdc2ce68e8a647bf567951cb73ba1b73
mads <mh@altapay.com>
2014-05-27 07:51:05 +0000

    [89] Removed Com classes, Navision examples and the MotoGui

--- AltaPayMoto/TestFormApp/Program.cs
+++ AltaPayMoto/TestFormApp/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Windows.Forms;
 using AltaPay;
 using AltaPay.Service;
-using AltaPay.Moto.Com;
 
 namespace TestFormApp
 {
@@ -16,44 +15,8 @@ namespace TestFormApp
         [STAThread]
         static void Main()
         {
-			Moto moto = new Moto();
-			moto.Initialize("http://gateway4.dev.earth.pensio.com/merchant.php/API/", "shop api", "testpassword", "AltaPay Soap Test Terminal", Guid.NewGuid().ToString(), 42.42, 208, "payment");
-
-			Merchant api = new Merchant();
-			api.Initialize("http://gateway4.dev.earth.pensio.com/merchant.php/API/", "shop api", "testpassword", "AltaPay Soap Test Terminal");
-
-			//api.CaptureWithIdentifier("73", 10, "whatisthis");
-			//api.RefundWithIdentifier("73", 10, "whatisthis2");
-
-			//IComFundingsResult result = api.GetFundings(1);
-			//MessageBox.Show(result.getFunding(0).getLine(0).ExchangeRate);
-			moto.Show();
-			int a = 0;
-			a++;
-			/*
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MyApplicationContext());
-			 * */
+		
         }
     }
 
-	public class MyApplicationContext : ApplicationContext
-	{
-		public MyApplicationContext()
-		{
-			
-			/*
-			moto.AddCreditCard("411100******0000", "156d557b225920dc3a231f777e44c975c1e6fe70");
-			moto.SetAvsInfo("my first name", "my last name", "Albertslund", "my postal code", "my city", "my region", "my country", "my phone", "my email");
-
-			IComPaymentResult result = moto.Show();
-
-			api.GetFundings(0);
-
-			MessageBox.Show(result.Result.ToString() + " Merchant message:"  + result.ResultMessage +  " avs message:" + result.Payment.AddressVerification + " " + result.Payment.AddressVerificationDescription);
-			*/
-			//MessageBox.Show(api.Split(result.Payment.PaymentId, 10.66).Payment.PaymentId);
-		}
-	}
 }
