f6d031464eda1cee3810e990efd0ff7f0f60652e
patrick <pt@altapay.com>
2014-09-13 11:01:05 +0000

    [36] Added request classes for MultiPaymentRequest. Also removed some unused "usings".

---
+++ AltaPayApi/AltaPayApi/Service/Requests/Common/MultiPaymentRequestRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AltaPay.Service
+{
+	public class MultiPaymentRequestRequest
+	{
+		public MultiPaymentRequestRequest()
+		{
+			this.Children = new List<MultiPaymentRequestRequestChild>();
+		}
+
+		// Required Parameters
+		public string Terminal { get; set; }
+		public string ShopOrderId { get; set; }
+		public IList<MultiPaymentRequestRequestChild> Children { get; private set; }
+
+		// Optional parameters
+		public string Language { get; set; }
+		public IDictionary<string,object> PaymentInfos { get; set; }
+		public AuthType Type { get; set; }
+		public string CreditCardToken { get; set; }
+		public string Cookie { get; set; }
+		public PaymentRequestConfig Config { get; set; }
+
+		public MultiPaymentRequestRequest AddChild(MultiPaymentRequestRequestChild child)
+		{
+			this.Children.Add(child);
+			return this;
+		}
+	}
+}
+
---
+++ AltaPayApi/AltaPayApi/Service/Requests/Common/MultiPaymentRequestRequestChild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AltaPay.Service
+{
+	public class MultiPaymentRequestRequestChild
+	{
+		// Required
+		public Amount Amount { get; set; }
+
+		// Optional
+		public string Terminal { get; set; }
+		public string ShopOrderId { get; set; }
+		public IDictionary<string,object> PaymentInfos { get; set; }
+		public AuthType Type { get; set; }
+		public ShippingType ShippingType { get; set; }
+		public string SalesReconciliationIdentifier { get; set; }
+		public IList<PaymentOrderLine> OrderLines { get; set;}
+	}
+}
