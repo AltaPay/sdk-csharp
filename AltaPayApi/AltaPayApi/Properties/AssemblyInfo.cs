e177a4e8f3dcaac226efc9a0f40a7cfe24f46c1b
patrick <pt@altapay.com>
2015-02-18 06:45:36 +0000

    [129] "Converted" the makepackage script to an ant build. Also changed the versioning of the dll, so we follow the same scheme as everywhere else.

--- AltaPayApi/AltaPayApi/Properties/AssemblyInfo.cs
+++ AltaPayApi/AltaPayApi/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1401c65c-1a7c-11df-a290-0009df8114c2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.3")]
-[assembly: AssemblyFileVersion("3.10.3")]
---
+++ AltaPayApi/AltaPayApi/Properties/SdkVersion.cs
@@ -0,0 +1,4 @@
+using System;
+using System.Reflection;
+[assembly: AssemblyVersion("2015.02.18")]
+[assembly: AssemblyFileVersion("2015.02.18")]
