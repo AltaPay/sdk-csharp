ddfff5436e64c8b027c7a68fe25e9936ac8af6b4
emerson <et@altapay.com>
2016-06-20 17:13:12 +0200

    [p20] Integration test for the method MerchantApi.GetFundingContent

--- AltaPayApi/AltaPayApi.Tests/Integration/FundingListTests.cs
+++ AltaPayApi/AltaPayApi.Tests/Integration/FundingListTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using AltaPay.Service;
 using NUnit.Framework;
 using AltaPay.Api.Tests;
+using AltaPay.Service.Dto;
 
 namespace AltaPay.Service.Tests.Integration
 {
@@ -23,11 +24,48 @@ namespace AltaPay.Service.Tests.Integration
 		[Test]
 		public void CallingMerchantApiWithSuccessfulParametersReturnsSuccessfulResult()
 		{
-			FundingsResult result = _api.GetFundings( new GetFundingsRequest { Page = 0});
+			FundingsResult result = _api.GetFundings(new GetFundingsRequest { Page = 0 });
 
 			Assert.AreEqual(Result.Success, result.Result);
 			Assert.AreEqual(1, result.Pages);
-			Assert.IsTrue(result.Fundings.Count>=1);
+			Assert.IsTrue(result.Fundings.Count >= 1);
+		}
+
+		[Test]
+		public void FundingDownloadTest()
+		{
+			FundingsResult result = _api.GetFundings(new GetFundingsRequest { Page = 0 });
+
+			Assert.AreEqual(Result.Success, result.Result);
+			Assert.AreEqual(1, result.Pages);
+			Assert.IsTrue(result.Fundings.Count >= 1);
+
+			Funding funding = result.Fundings[0];
+
+			FundingContentResult fres = _api.GetFundingContent(funding);
+
+			FundingRecord record = fres.GetFundingRecordList().ElementAt(0);
+
+			Assert.AreEqual(new DateTime(2010, 12, 24), record.FundingDate);
+			Assert.AreEqual("fee", record.RecordType);
+			Assert.AreEqual("Monthly fee", record.Id);
+			Assert.AreEqual("", record.ReconciliationIdentifier);
+			Assert.AreEqual("", record.PaymentId);
+			Assert.AreEqual("", record.OrderId);
+			Assert.AreEqual("", record.Terminal);
+			Assert.AreEqual("AltaPay Functional Test Shop", record.Shop);
+
+			Amount amount = Amount.Get(0.0M, Currency.EUR);
+
+			Assert.AreEqual(amount, record.PaymentAmount);
+			Assert.AreEqual(1.0M, record.ExchangeRate);
+			Assert.AreEqual(amount, record.FundingAmount);
+			Assert.AreEqual(amount, record.FixedFeeAmount);
+			Assert.AreEqual(amount, record.FixedFeeVatAmount);
+			Assert.AreEqual(amount, record.RateBasedFeeAmount);
+			Assert.AreEqual(amount, record.RateBasedFeeVatAmount);
+
+
 		}
 	}
 }
