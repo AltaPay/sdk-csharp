7f32eb424e80949544b73b96aad50e50cad50c65
emerson <et@altapay.com>
2016-06-21 16:26:37 +0200

    [p12] Change in class InvoiceReservationRequest Class InvoiceReservationRequest now extends BasePaymentRequestRequest

--- AltaPayApi/AltaPayApi/Service/Requests/Common/InvoiceReservationRequest.cs
+++ AltaPayApi/AltaPayApi/Service/Requests/Common/InvoiceReservationRequest.cs
@@ -3,22 +3,17 @@ using System.Collections.Generic;
 
 namespace AltaPay.Service
 {
-	public class InvoiceReservationRequest
+	public class InvoiceReservationRequest : BasePaymentRequestRequest
 	{
 
 		// required
-		public string Terminal { get; set; }
-		public string ShopOrderId { get; set; }
-		public Amount Amount { get; set; }
-		//public Currency Currency { get; set; }
 		public CustomerInfo CustomerInfo { get; set; }
 
 		// Optional parameters
 		public AuthType? AuthType { get; set; }
-		public IDictionary<string,object> PaymentInfos { get; set; } // transaction infos
 		public string AccountNumber { get; set; }
 		public string BankCode { get; set; }
-		public FraudService? FraudService { get; set; }
+		public new FraudService? FraudService { get; set; }
 		public PaymentSource? PaymentSource { get; set; }
 		public IList<PaymentOrderLine> OrderLines { get; set; }
 		public string OrganisationNumber { get; set; } 
