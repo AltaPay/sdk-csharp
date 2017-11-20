f8052ce7a5d9da3de0d2846afd735284e162e8ec
Milan Miljkovic <mi.miljkovic@levi9.com>
2017-11-20 15:04:23 +0100

    [VESPLG-188] - Added createPaymentRequest, capture, refund and release payment examples

---
+++ AltaPayApi/Examples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Examples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ReleaseExamples releaseExamples = new ReleaseExamples();
+            //releaseExamples.Release();
+
+            //RefundExamples refundExamples = new RefundExamples();
+            //refundExamples.SimpleRefund();
+
+            CaptureExamples captureExamples = new CaptureExamples();
+            captureExamples.SimpleCapture();
+            captureExamples.SimplePartialCapture();
+            captureExamples.ComplexCapture();
+        }
+    }
+}
