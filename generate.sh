60d125f0f924a4b970605801faf171192f8ee054
lars <lb@altapay.com>
2018-03-19 12:59:06 +0100

    [VESPLG-507] Update source for code generation.

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget "http://gateway.dev.pensio.com/APIResponse.xsd" -OAPIResponse.xsd
+wget "http://cigateway.mars.pensio.com/APIResponse.xsd" -OAPIResponse.xsd
 xsd APIResponse.xsd /c /o:AltaPayApi/AltaPayApi/Generated /namespace:AltaPay.Service.Dto
 rm APIResponse.xsd
