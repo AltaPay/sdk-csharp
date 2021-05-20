fd70e29ae47c3a956cb7d2eadf9b6f5e5dfc6a0f
Farhad Kabir <fk@embrace-it.com>
2021-05-20 18:22:06 +0500

    fix bash script to run from anywhere

--- docker/docker-build-script.sh
+++ docker/docker-build-script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker build . --file Dockerfile-build-image -t csharp-build
-docker run --rm --mount type=bind,source="$(pwd)/..",target=/app csharp-build ../bin/sh -c 'cd /app && ant'
+set -e
+
+SOURCEPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo ${SOURCEPATH}/..
+
+cd ${SOURCEPATH}/..
+
+docker build . --file docker/Dockerfile-build-image -t csharp-build
+docker run --rm --mount type=bind,source="$(pwd)",target=/app csharp-build ../bin/sh -c 'cd /app && ant'
